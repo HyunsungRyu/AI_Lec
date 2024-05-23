47d8abc12374ed608b5c2d99f65e2581cca5eb8d
HyunsungRyu <128374366+HyunsungRyu@users.noreply.github.com>
2024-05-23 16:03:17 +0900

    Update program01.py

--- program01.py
+++ program01.py
@@ -13,4 +13,4 @@ with st.form('my_form'):
     st.warning('OpenAI API 인증키를 입력해 주세요!', icon='⚠')
   if submitted and openai_api_key.startswith('sk-'):
     
-    generate_response(text)
+    generate_response("너는 여행과 관련된 것만 말해야해\n질문:"+text)
